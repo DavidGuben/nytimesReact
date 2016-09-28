bfb247aa890a9b1f621e61c2e6ba9dae732fd335
DavidGuben <davidjg6@gmail.com>
2016-09-28 18:41:55 -0400

    starting html setup

--- App.js
+++ App.js
@@ -14,10 +14,6 @@ class App extends React.Component {
     return (
     <div>
       <Widget txt={this.state.txt} update={this.update} />
-      <Widget txt={this.state.txt} update={this.update} />
-      <Widget txt={this.state.txt} update={this.update} />
-      <Widget txt={this.state.txt} update={this.update} />
-      <Widget txt={this.state.txt} update={this.update} />
     </div>
     );
   }
@@ -26,9 +22,27 @@ class App extends React.Component {
 const Widget = (props) => {
   return (
     <div>
-      <input type="text"
+      <div className="jumbotron">
+        <h1>NY Times Article Search</h1>
+        <p>Search articles of interest</p>
+      </div>
+      <div className="panel panel-primary">
+        <div className="panel-heading">
+          <h3 className="panel-title">Search</h3>
+      </div>
+      <h1>{props.txt}</h1>
+      <div className="panel-body">
+        <p>Topic</p>
+        <input type="text"
         onChange={props.update} />
-        <h1>{props.txt}</h1>
+        <p>Start Year</p>
+        <input type="text"
+        onChange={props.update} />
+        <p>End Year</p>
+        <input type="text"
+        onChange={props.update} />
+      </div>
+    </div>
     </div>
   );
 }
